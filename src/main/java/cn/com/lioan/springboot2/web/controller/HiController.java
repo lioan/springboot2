fea986e9552538e1d1ee17896fac368efe527124
lioan <690630267@qq.com>
2019-01-06 16:46:15 +0800

    add js

--- src/main/java/cn/com/lioan/springboot2/web/controller/HiController.java
+++ src/main/java/cn/com/lioan/springboot2/web/controller/HiController.java
@@ -19,21 +19,20 @@ public class HiController {
     @Autowired
     LioanerService lioanerService;
 
-    @RequestMapping(value = "/hi")
+    @RequestMapping(value = "/")
     public String hi() {
         return "hi,spring boot2.1.0";
     }
 
+    //测试mybatis
     @RequestMapping("/lioans")
     public List<Lioaner> getLioaners() {
         return lioanerService.getLioaners();
     }
-
     @RequestMapping("/lioans1")
     public List<Lioaner> getLioaners1() {
         return lioanerService.getLioaners1();
     }
-
     @RequestMapping("/lioaner/{id}")
     public Lioaner getLiaonerById(@PathVariable String id){
         return lioanerService.getLioanerById(id);
